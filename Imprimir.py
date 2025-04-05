71a6871d25f2a2ceb922536ae63110a5157fb722
angelovegazava20@gmail.com <angelovegazava20@gmail.com>
2025-04-04 19:39:17 -0500

    archivos imprimir e ingreso

---
+++ Imprimir.py
@@ -0,0 +1,15 @@
+a = "Carlos"
+b=20
+print("Hola ",a," tienes ", b)
+
+c= "Hola " + str(a) + " tienes " +str(b)
+print(c)
+
+d= f"Hola  {a} tienes {b+10}"
+print(d)
+
+
+
+print ("Carlos", "Jose", "Juan")
+print ("Carlos", "Jose", "Juan", sep="* ", end="%\n")
+print ("Carlos", "Jose", "Juan")
---
+++ Program.py
@@ -0,0 +1,17 @@
+a=2**3 +1
+
+if a == 8:
+    print (a)
+else:
+    print ("No")
+
+b = "hola" " " "amigo"
+print(b)
+
+c= "Estudia"*5
+print(c)
+
+# comentario una linea
+"""
+  texto varias lineas
+"""
